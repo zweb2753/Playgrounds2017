a64c81d65c1bdbcd064828dafbb7bf031d983188
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-09-25 08:31:45 -0600

    made some variables

--- learningSwift.playground/Contents.swift
+++ learningSwift.playground/Contents.swift
@@ -1,5 +1,23 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//explicit type declaration with no value
+var ramen :String
+//implit type decleration with a value to assign type
+var soup = "ramen"
+ramen = soup
+print(ramen)
+print(soup)
+var myAge :Int
+myAge = -89
+print(myAge)
+myAge = myAge + 54322456
+var someValue 3.1415 * 56
+someValue = someValue * 2331.1
+var is isReal :Bool
+isReal = false
+isReal = true:
+let myName :String
+myName = "Zane"
+print(myName)
 
-var str = "Hello, playground"
