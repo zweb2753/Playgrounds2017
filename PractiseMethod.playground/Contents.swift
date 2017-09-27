d2df273cb154451e72be6ff83e8ea301b19459a9
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-09-27 09:00:52 -0600

    finished writing methods for swift

--- PractiseMethod.playground/Contents.swift
+++ PractiseMethod.playground/Contents.swift
@@ -20,3 +20,31 @@ public func evenLessSimpleMethod(count :Int, bandName :String, date :String) -> 
 evenLessSimpleMethod(count: 4, bandName: "They might be giants", date: "june, 1992")
 
 
+public func calculateDays(first firstDay :Int, secondDay :Int) -> Int
+{
+    let answer = secondDay - firstDay
+    
+    return answer
+}
+calculateDays(first: 4, secondDay:5)
+public func name(friendName homeName :String) -> Void
+{
+    let info = "At home my name is" + homeName
+    print(info)
+}
+name(friendName: "Cody")
+
+var guess = 453
+if (guess < 500)
+{
+    print("wahoo")
+}
+else if(guess == 65)
+{
+    print("Not likely")
+}
+while (guess > 0)
+{
+    print("hahaha \(guess)")
+    guess -= 1
+}
