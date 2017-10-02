74be3ec16bca8758712b20a36de87847ab302e43
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2017-10-02 09:24:43 -0600

    did the counting string assignment.

---
+++ StringAssignment.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let newstr = "hello User"
+
+
+var name = "Zane"
+print("HI, \(name)!")
+
+var countingChar = "CodingClass"
+if countingChar.characters.count == 0
+{
+    countingChar = "n/a"
+}
+
+let string1 = "code is cool"
+let string2 = "school is boring"
+
+if string1>string2
+{
+    print("string 1 is first")
+}
+else
+{
+    print("string1 is second")
+}
